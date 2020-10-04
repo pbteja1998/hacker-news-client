b7e8c10a2a5d3131602bb6002dbce6a564f0c3f9
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-04 10:43:04 +0530

    Add metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,21 +4,62 @@ import Document, {
   Main,
   NextScript,
   DocumentContext,
-} from 'next/document';
+} from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
     return (
       <Html lang='en'>
         <Head>
-          <link
-            rel='stylesheet'
-            href='https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css'
+          <meta charSet={'utf-8'} />
+          <meta name={'application-name'} content={'Hacker News'} />
+          <meta name={'twitter:card'} content={'summary'} />
+          <meta name={'twitter:creator'} content={'@pbteja1998'} />
+          <meta property={'og:type'} content={'website'} />
+          <meta name={'author'} content={'Bhanu Teja P'} />
+          <meta property={'og:site_name'} content={'Hacker News'} />
+          <meta name={'theme-color'} content={'#d03801'} />
+          <meta
+            name={'description'}
+            content={
+              'A Hacker News client to view stories from news.ycombinator.com.'
+            }
+          />
+          <meta property={'og:title'} content={'Hacker News'} />
+          <meta
+            property={'og:description'}
+            content={
+              'A Hacker News client to view stories from news.ycombinator.com.'
+            }
+          />
+          <meta property={'og:url'} content={'https://hn.bhanuteja.dev'} />
+          <meta property='twitter:title' content={'Hacker News'} />
+          <meta
+            property='twitter:description'
+            content={
+              'A Hacker News client to view stories from news.ycombinator.com.'
+            }
+          />
+          <meta property='twitter:url' content={'https://hn.bhanuteja.dev'} />
+          <meta property={'og:image'} content={'/preview.png'} />
+          <meta property='twitter:image' content={'/preview.png'} />
+          <meta
+            name={'viewport'}
+            content={
+              'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover'
+            }
+          />
+          <link rel={'shortcut icon'} href={'/favicon.ico'} />
+          <script
+            src='https://cdn.usefathom.com/script.js'
+            // @ts-ignore
+            site={'BBOGJGAJ'}
+            defer
           />
         </Head>
         <body>
@@ -26,8 +67,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
